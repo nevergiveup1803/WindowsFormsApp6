14f43be300715b63f4a71d00576ea096ac4ea767
Kodzaev <kodzaev-marat@mail.ru>
2019-05-15 09:56:09 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.labelTimeStart = new System.Windows.Forms.Label();
+            this.labelTimeEnd = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(150, 128);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(164, 87);
+            this.button.TabIndex = 0;
+            this.button.Text = "запустить функцию";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelTimeStart
+            // 
+            this.labelTimeStart.AutoSize = true;
+            this.labelTimeStart.Location = new System.Drawing.Point(387, 128);
+            this.labelTimeStart.Name = "labelTimeStart";
+            this.labelTimeStart.Size = new System.Drawing.Size(35, 13);
+            this.labelTimeStart.TabIndex = 1;
+            this.labelTimeStart.Text = "label1";
+            // 
+            // labelTimeEnd
+            // 
+            this.labelTimeEnd.AutoSize = true;
+            this.labelTimeEnd.Location = new System.Drawing.Point(387, 202);
+            this.labelTimeEnd.Name = "labelTimeEnd";
+            this.labelTimeEnd.Size = new System.Drawing.Size(35, 13);
+            this.labelTimeEnd.TabIndex = 2;
+            this.labelTimeEnd.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTimeEnd);
+            this.Controls.Add(this.labelTimeStart);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label labelTimeStart;
+        private System.Windows.Forms.Label labelTimeEnd;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
